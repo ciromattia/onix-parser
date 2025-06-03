cd57f43f395d1ceb70e22905c7ca88d123de64d2
Julio Braña Bouza <julio@alley.co>
2025-06-03 07:19:27 +0300

    update codelist220

--- src/CodeList/CodeList220.php
+++ src/CodeList/CodeList220.php
@@ -18,6 +18,7 @@ class CodeList220 extends CodeList implements CodeListInterface
 	 * @see https://ns.editeur.org/onix/en/220
 	 */
 	protected static $en = [
+		'06'   => 'Not FSC or PEFC certified',
 		'101A' => 'EPUB 2.0.1',
 		'101B' => 'EPUB 3.0',
 		'101C' => 'EPUB 3.0.1',
@@ -36,6 +37,7 @@ class CodeList220 extends CodeList implements CodeListInterface
 	 * @see https://ns.editeur.org/onix/es/220
 	 */
 	protected static $es = [
+		'06'   => 'Sin certificación FSC o PEFC',
 		'101A' => 'EPUB 2.0.1',
 		'101B' => 'EPUB 3.0',
 		'101C' => 'EPUB 3.0.1',
@@ -54,6 +56,7 @@ class CodeList220 extends CodeList implements CodeListInterface
 	 * @see https://ns.editeur.org/onix/de/220
 	 */
 	protected static $de = [
+		'06'   => 'Nicht FSC oder PEFC zertifiziert',
 		'101A' => 'EPUB 2.0.1',
 		'101B' => 'EPUB 3.0',
 		'101C' => 'EPUB 3.0.1',
@@ -72,6 +75,7 @@ class CodeList220 extends CodeList implements CodeListInterface
 	 * @see https://ns.editeur.org/onix/fr/220
 	 */
 	protected static $fr = [
+		'06'   => 'Non certifié FSC ou PEFC',
 		'101A' => 'EPUB 2.0.1',
 		'101B' => 'EPUB 3.0',
 		'101C' => 'EPUB 3.0.1',
@@ -90,6 +94,7 @@ class CodeList220 extends CodeList implements CodeListInterface
 	 * @see https://ns.editeur.org/onix/it/220
 	 */
 	protected static $it = [
+		'06'   => 'Non certificato FSC o PEFC',
 		'101A' => 'EPUB 2.0.1',
 		'101B' => 'EPUB 3.0',
 		'101C' => 'EPUB 3.0.1',
@@ -108,6 +113,7 @@ class CodeList220 extends CodeList implements CodeListInterface
 	 * @see https://ns.editeur.org/onix/nb/220
 	 */
 	protected static $nb = [
+		'06'   => 'Ikke FSC eller PEFC-sertifisert',
 		'101A' => 'EPUB 2.0.1',
 		'101B' => 'EPUB 3.0',
 		'101C' => 'EPUB 3.0.1',
@@ -126,6 +132,7 @@ class CodeList220 extends CodeList implements CodeListInterface
 	 * @see https://ns.editeur.org/onix/tr/220
 	 */
 	protected static $tr = [
+		'06'   => 'FSC veya PEFC sertifikasız',
 		'101A' => 'EPUB 2.0.1',
 		'101B' => 'EPUB 3.0',
 		'101C' => 'EPUB 3.0.1',
